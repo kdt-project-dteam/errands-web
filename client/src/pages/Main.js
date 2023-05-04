74afb87d539af275e1ca20d5e2abc6b877d2ff6c
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-05-04 17:42:45 +0900

    server branch pull을 위한 사전커밋

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -2,17 +2,22 @@ import React from 'react';
 import '../css/main.scss';
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { asyncUpAxios, asyncUpAxios2 } from '../store/testCounter';
+import { Link } from 'react-router-dom';
 
 export default function Main() {
     const dispatch = useDispatch();
+    // redux 로딩처리를 위한 변수
     const status = useSelector((state) => {
         return state.someReducer.status
     })
+    // redux 에 저장된 게시판 json데이터 가져오기
     const value = useSelector(state => {
         return state.someReducer.value
     })
+    // 구인구직 게시판 상태 [offer : 구인] , [search : 구직]
+    const [boardState, setBoardState] = useState('offer');
     useEffect(() => {
         dispatch(asyncUpAxios())
     }, [])
@@ -26,20 +31,22 @@ export default function Main() {
                     <ul>
                         {value ? value.map((data) => {
                             return (
-                                <li>
-                                    <div className='li-top'>
-                                        <div className='li-top-title'>{data.email}</div>
-                                        <div className='li-top-author'>{data.name}</div>
-                                    </div>
-                                    <div className='li-bottom'>
-                                        <div className='li-bottom-time'>
-                                            {data.email}
+                                <Link to={'/boardDetail/:boardId'}>
+                                    <li>
+                                        <div className='li-top'>
+                                            <div className='li-top-title'>{data.email}</div>
+                                            <div className='li-top-author'>{data.name}</div>
                                         </div>
-                                        <div className='li-bottom-hit'>
-                                            {data.postId}
+                                        <div className='li-bottom'>
+                                            <div className='li-bottom-time'>
+                                                {data.email}
+                                            </div>
+                                            <div className='li-bottom-hit'>
+                                                {data.postId}
+                                            </div>
                                         </div>
-                                    </div>
-                                </li>
+                                    </li>
+                                </Link>
                             )
                         }) : 'null'}
                     </ul>
@@ -48,10 +55,10 @@ export default function Main() {
             <div className='main-right'>
                 <h2>
                     <div>급구!✈️</div>
-                    <div className='ft-08 card'>
+                    <div className='ft-08 card' onClick={() => { setBoardState('offer') }}>
                         구인
                     </div>
-                    <div className='ft-08 card'>
+                    <div className='ft-08 card' onClick={() => { setBoardState('search') }}>
                         구직
                     </div>
                 </h2>
--- client/src/pages/Members.js
+++ client/src/pages/Members.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { asyncUpAxios, asyncUpAxios2 } from '../store/testCounter';
 
 export default function Members() {
     return (
         <div className='Members'>
+
         </div>
     )
 }
