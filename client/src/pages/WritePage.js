49e71115cc6ec3d6b373f53c9fbff6e1c6351693
마상위 <ma0719@naver.com>
2023-05-13 16:44:51 +0900

    글작성 pull 받기위한 commit

--- client/src/pages/WritePage.js
+++ client/src/pages/WritePage.js
@@ -28,7 +28,7 @@ export default function WritePage({ data }) {
       method: "post",
       url: `${process.env.REACT_APP_DB_HOST}/api/wanter`,
       data: {
-        user_name: localStorage.getItem('userName'),
+        user_name: localStorage.getItem("userName"),
         wanter_board_title: title,
         wanter_board_content: content,
         wanter_board_dead_line: initialDate + " " + detailDate,
@@ -36,8 +36,8 @@ export default function WritePage({ data }) {
         wanter_board_place_detail: detailAddress,
       },
     });
-    console.log(localStorage.getItem('userName'))
-    console.log(result)
+    console.log(localStorage.getItem("userName"));
+    console.log(result);
   };
 
   const submitSeeker = async (e) => {
@@ -45,7 +45,7 @@ export default function WritePage({ data }) {
       method: "post",
       url: `${process.env.REACT_APP_DB_HOST}/api/helper`,
       data: {
-        user_name: localStorage.getItem('userName'),
+        user_name: localStorage.getItem("userName"),
         helper_board_title: title,
         helper_board_content: content,
         helper_board_place: address,
