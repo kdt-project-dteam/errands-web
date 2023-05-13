5f9b9b40ec011d7ca1eee9e0ebab043b7491fa55
glad-hee <illesteva0@naver.com>
2023-05-13 18:58:04 +0900

    fix

--- server/routes/chat.js
+++
@@ -1,7 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-// const Socket = require("../controller/Cchat");
-
-// router.get("/", Socket.Socket);
-
-// module.exports = router;
