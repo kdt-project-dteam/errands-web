4624a40e6b1e898003edcb4732ed7e881e41a957
마상위 <ma0719@naver.com>
2023-05-13 19:04:39 +0900

    front 기능 수정및 추가

--- client/src/pages/BoardDetail.js
+++ client/src/pages/BoardDetail.js
@@ -13,6 +13,7 @@ import KakaoMap from "../components/KakaoMap";
 export default function BoardDetail() {
   const nowTime = moment().format("YYYY-MM-DD HH:mm:ss");
   const [inputCount, setInputCount] = useState(0);
+  const [valueCount, setValueCount] = useState("");
   const value = useSelector((state) => {
     return state.someReducer.value;
   });
@@ -163,7 +164,12 @@ export default function BoardDetail() {
                           type="button"
                           onClick={() => {
                             inputCount == 0
-                              ? alert("한글자 이상 입력하세요!")
+                              ? Swal.fire({
+                                  icon: "error",
+                                  title: `한글자 이상 입력하세요!`,
+                                  showConfirmButton: false,
+                                  timer: 1500,
+                                })
                               : sendCommentData();
                           }}
                           className="comment_submit"
@@ -256,13 +262,21 @@ export default function BoardDetail() {
                     <textarea
                       className="comment_textarea"
                       onChange={inputChange}
+                      maxLength={200}
                     ></textarea>
                     <div className="comment_submit_form">
-                      <span className="comment_count">0/100</span>
+                      <span className="comment_count">{inputCount}/200</span>
                       <button
                         type="button"
                         onClick={() => {
-                          sendCommentData();
+                          inputCount == 0
+                            ? Swal.fire({
+                                icon: "error",
+                                title: `한글자 이상 입력하세요!`,
+                                showConfirmButton: false,
+                                timer: 1500,
+                              })
+                            : sendCommentData();
                         }}
                         className="comment_submit"
                       >
